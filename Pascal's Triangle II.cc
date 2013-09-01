f424380bfc6cc2ed39ac8df30f8549e93a952ba1
Dionysus1227 <Dionysus1227@users.noreply.github.com>
2013-09-01 10:52:33 +0800

    Create Pascal's Triangle II.cc

---
+++ Pascal's Triangle II.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+       vector<int> ans;       
+       for (int i = 0; i < rowIndex; i++) {
+            ans.push_back(1);
+            for (int j = ans.size() - 1; j > 0; j--)
+                ans[j] += ans[j - 1];
+       }
+        return ans;
+    }
+};
