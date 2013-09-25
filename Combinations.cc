4f85aa8698f8ad884bfc69a8f12826f413ab41e7
Dionysus1227 <Dionysus1227@users.noreply.github.com>
2013-09-25 16:41:42 +0800

    Update Combinations.cc

--- Combinations.cc
+++ Combinations.cc
@@ -19,3 +19,27 @@ private:
         }
     }
 };
+//non-recursion version
+class Solution {
+public:
+    vector<vector<int> > combine(int n, int k) {
+        vector<vector<int> > ans;
+        int i = 0;
+        vector<int> cur(k, 0);
+        while (i >= 0) {
+            if (i == k) {
+                ans.push_back(cur);
+                i--;
+                continue;
+            }
+            if (cur[i] == 0) {
+                if (i == 0) cur[i] = 1;
+                else cur[i] = cur[i - 1] + 1;
+            } else cur[i]++;
+            if (cur[i] > n) {
+                cur[i--] = 0;
+            } else i++;
+        }
+        return ans;
+    }
+};
