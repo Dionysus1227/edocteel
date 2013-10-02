3c24fdf8f995b5eda87a8e3159d1ab949f8a725d
Dionysus1227 <Dionysus1227@users.noreply.github.com>
2013-10-02 13:32:19 +0800

    Update Candy.cc

--- Candy.cc
+++ Candy.cc
@@ -22,3 +22,32 @@ public:
         return tot;
     }
 };
+
+//Solution with constant space
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        if (ratings.size() == 0) return 0;
+        int n = ratings.size();
+        int tot = 0;
+        int pre_value = 0;
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || ratings[i - 1] < ratings[i]) {
+                ++cur;
+                if (i + 1 == n || ratings[i] <= ratings[i + 1])
+                    tot += cur;
+                else pre_value = cur;
+            } else if (i + 1 == n || ratings[i] <= ratings[i + 1]) {
+                int j = i;
+                for (cur = 1; j > 0 && ratings[j - 1] > ratings[j]; j--, cur++)
+                    tot += cur;
+                if (j > 0 && ratings[j - 1] < ratings[j]) cur = max(cur, pre_value);
+                tot += cur;
+                pre_value = 1;
+                cur = 1;
+            }
+        }
+        return tot;
+    }
+};
